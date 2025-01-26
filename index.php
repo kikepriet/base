54fb4dbadaf78f398a884b4e4a127eb03bdb5e3e
kikepriet <kikepriet@gmail.com>
2025-01-26 01:12:04 -0600

    agregando cdns al index

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!--CDNs-->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.15.10/dist/sweetalert2.all.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.15.10/dist/sweetalert2.min.css" rel="stylesheet">
+<script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+
+
     <title>Base</title>
 </head>
 <body>
-    
+    <h1 class="text-4xl font-bold text-sky-600 text-center mt-4">Hola</h1>
 </body>
 </html>
